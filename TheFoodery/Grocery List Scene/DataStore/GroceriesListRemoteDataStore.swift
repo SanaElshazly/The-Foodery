510f28bb28263d12f55389bad3e27712c9c8a9d1
Sana <sanaelshazly1993@gmail.com>
2019-06-07 06:51:10 +0200

    Add groceries list remote data store

---
+++ TheFoodery/Grocery List Scene/DataStore/GroceriesListRemoteDataStore.swift
@@ -0,0 +1,33 @@
+//
+//  GroceriesListRemoteDataStore.swift
+//  TheFoodery
+//
+//  Created by Sana Elshazly on 6/7/19.
+//  Copyright © 2019 Sana Elshazly. All rights reserved.
+//
+
+import Foundation
+
+protocol GroceriesListRemoteDataStoreProtocol {
+    func fetchGroceries(completion: @escaping (Result<GroceryData, NetworkError>) -> ())
+    
+}
+
+class GroceriesListRemoteDataStore: GroceriesListRemoteDataStoreProtocol {
+    private let sessionProvider = URLSessionHandler()
+    
+    func fetchGroceries(completion: @escaping (Result<GroceryData, NetworkError>) -> ()) {
+        sessionProvider.request(type: GroceryData.self, service: GroceryRequest.groceries) { (response) in
+            switch response {
+            case let .success(groceries):
+                print(groceries)
+                completion(.success(groceries))
+                
+            case let .failure(error):
+                completion(.failure(.failed))
+                print(error)
+            }
+        }
+        
+    }
+}
