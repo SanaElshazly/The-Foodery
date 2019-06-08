025c6543d2efaf1991e99f54c00c833ad33f456e
Sana <sanaelshazly1993@gmail.com>
2019-06-08 05:48:06 +0200

    Create custom cell for product item

---
+++ TheFoodery/Grocery List Scene/CustomViews/GroceryItemCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  GroceryItemCollectionViewCell.swift
+//  TheFoodery
+//
+//  Created by Sana Elshazly on 6/7/19.
+//  Copyright © 2019 Sana Elshazly. All rights reserved.
+//
+
+import UIKit
+import Reusable
+import SDWebImage
+
+class GroceryItemCollectionViewCell: UICollectionViewCell, NibReusable {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(with product: Product?)  {
+        guard product != nil else {
+            return
+        }
+
+        nameLabel.text = product?.name
+        priceLabel.text = "\(product?.price)"
+        productImageView.sd_setImage(with: URL(string: product?.image ?? "placeholder.png"))
+    }
+    
+}
+
