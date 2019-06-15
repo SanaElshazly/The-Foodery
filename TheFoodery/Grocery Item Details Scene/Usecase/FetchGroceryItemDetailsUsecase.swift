ef9bf0b45f52a8f3fc3b0964531a37fe2e960d8c
Sana <sanaelshazly1993@gmail.com>
2019-06-16 00:38:45 +0200

    Implement fetchGroceryItemDetailsUsecase

--- TheFoodery/Grocery Item Details Scene/Usecase/FetchGroceryItemDetailsUsecase.swift
+++ TheFoodery/Grocery Item Details Scene/Usecase/FetchGroceryItemDetailsUsecase.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class FetchGroceryItemDetailsUsecase {
+    
+    private let sessionProvider = URLSessionHandler()
+    private var productID = ""
+    
+    func setProductID(_ productID: String) {
+        self.productID = productID
+    }
+    
+    func fetchGroceryItemDetails(completion: @escaping (Result<Product, NetworkError>) -> ()) {
+        sessionProvider.request(type: Product.self, service: GroceryRequest.productDetail(product_id: productID)) { (response) in
+            switch response {
+            case let .success(product):
+                print(product)
+                completion(.success(product))
+
+            case let .failure(error):
+                completion(.failure(.failed))
+                print(error)
+            }
+        }
+        
+    }
+    
+}
