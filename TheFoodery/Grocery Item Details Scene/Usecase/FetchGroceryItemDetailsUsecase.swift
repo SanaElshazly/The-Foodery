21db654db3577a1796dffad50e4f486a34b213b5
Sana <sanaelshazly1993@gmail.com>
2019-06-16 00:20:09 +0200

    Add base classes for grocery item details scene

---
+++ TheFoodery/Grocery Item Details Scene/Usecase/FetchGroceryItemDetailsUsecase.swift
@@ -0,0 +1,9 @@
+//
+//  FetchGroceryItemDetailsUsecase.swift
+//  TheFoodery
+//
+//  Created by Sana Elshazly on 6/16/19.
+//  Copyright © 2019 Sana Elshazly. All rights reserved.
+//
+
+import Foundation
---
+++ TheFoodery/Grocery Item Details Scene/ViewController/GroceryItemDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroceryItemDetailsViewController.swift
+//  TheFoodery
+//
+//  Created by Sana Elshazly on 6/16/19.
+//  Copyright © 2019 Sana Elshazly. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItemDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
